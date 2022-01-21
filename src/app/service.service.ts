922d8fae762f8206b0004038576654b80c80e539
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-01-21 15:35:10 -0300

    commit modelo de produtos

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produtos } from './components/produtos.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
+  urlBase: string = "http://localhost:3001/produtos";
   sidenav_opened = true;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   acaoNav(valor: boolean): Boolean{
     return this.sidenav_opened = valor;
   }
+
+  buscarProdutos(): Observable<Produtos[]>{
+    return this.http.get<Produtos[]>(this.urlBase)
+  }
+
 }
