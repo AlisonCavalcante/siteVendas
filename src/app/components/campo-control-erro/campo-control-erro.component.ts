efa0eaa61dcde2cde0770d49836595236925d8be
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-02-25 21:10:48 -0300

    criação do componente de mensagem de erro

---
+++ src/app/components/campo-control-erro/campo-control-erro.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campo-control-erro',
+  templateUrl: './campo-control-erro.component.html',
+  styleUrls: ['./campo-control-erro.component.css']
+})
+export class CampoControlErroComponent implements OnInit {
+
+  @Input() msgErro!: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
