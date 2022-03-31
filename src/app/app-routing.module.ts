61da9e7c2cfc6bb05fb0192f65f51bb28b4390b8
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-31 09:08:25 -0300

    add cadastro componente no módulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { FormNovoEnderecoComponent } from './views/form-novo-endereco/form-novo-
 import { CosmeticosPageComponent } from './views/cosmeticos-page/cosmeticos-page.component';
 import { MaterialEscolarPageComponent } from './views/material-escolar-page/material-escolar-page.component';
 import { EletrodomesticosPageComponent } from './views/eletrodomesticos-page/eletrodomesticos-page.component';
-import { CadastroComponent } from './cadastro/cadastro/cadastro.component';
+
 import { HomeComponent } from './views/home/home.component';
 import { EletronicosPageComponent } from './views/eletronicos-page/eletronicos-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CestaComprasComponent } from './views/cesta-compras/cesta-compras.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
@@ -34,10 +35,10 @@ const routes: Routes = [
     path: 'cesta',
     component: CestaComprasComponent,
   },
-  // {
-  //   path: 'cadastro',
-  //   component: CadastroComponent,
-  // },
+   {
+     path: 'cadastro',
+     component: CadastroComponent,
+   },
   {
     path: 'cosmetico',
     component: CosmeticosPageComponent,
--- src/app/cadastro/cadastro.module.ts
+++ src/app/cadastro/cadastro.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CadastroComponent } from './cadastro.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CadastroComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: []
 })
 export class CadastroModule { }
