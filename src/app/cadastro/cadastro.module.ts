7ca47d3d30af4c697a23c4aeb66f300722717767
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-31 10:04:49 -0300

    add imports necessários do angular materiall

--- src/app/cadastro/cadastro.module.ts
+++ src/app/cadastro/cadastro.module.ts
@@ -7,6 +7,10 @@ import { CriarContaComponent } from './criar-conta/criar-conta.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {CadastroRoutingModule } from './cadastro.routing.module';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import {MatSelectModule} from '@angular/material/select';
     CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    CadastroRoutingModule,
+    MatDividerModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [CriarContaComponent]
 })
