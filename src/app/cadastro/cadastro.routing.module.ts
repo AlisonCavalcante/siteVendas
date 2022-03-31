0bc038a5fc74dcafd478b9aa13bec3c056bd83d6
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-31 10:03:41 -0300

    creat rotas do módulo de cadastro

---
+++ src/app/cadastro/cadastro.routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { CadastroComponent } from './cadastro.component';
+import { CriarContaComponent } from './criar-conta/criar-conta.component';
+
+const cadastroRoutes: Routes = [
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'criarContaPage', component: CriarContaComponent,},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(cadastroRoutes)],
+  exports: [RouterModule],
+})
+export class CadastroRoutingModule {}
