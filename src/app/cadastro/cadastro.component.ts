3933f9c078e763333a9f52b9f2dfa5d31a3d0553
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-31 10:05:01 -0300

    add código cadastro componente

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  formulario!: FormGroup;
+
+  constructor(private route: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      login: [null, Validators.required],
+      senha: [null, Validators.required]
+    });
+  }
+  cadastro(){
+    this.route.navigate(["/criarContaPage"])
+  }
+  onSubmit(){
+
   }
 
 }
