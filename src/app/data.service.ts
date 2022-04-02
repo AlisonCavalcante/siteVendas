0a4607bcf550bff1848ccea03224d1927846254d
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-01 21:17:03 -0300

    create service geral

---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Produtos } from './components/produtos.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private produto!: Produtos[];
+  constructor() { }
+
+  setProduto(produto: Produtos[]){
+    this.produto = produto;
+  }
+
+  getProduto(){
+    return this.produto;
+  }
+}
