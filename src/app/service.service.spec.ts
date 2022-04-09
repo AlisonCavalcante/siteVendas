6603e96a28aff6a80aa3623a02a1270ba250e7ba
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-09 15:25:49 -0300

    craição de verificacção de usuário.

--- src/app/service.service.spec.ts
+++ src/app/service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceService } from './service.service';
+import { ProdutoService } from './service.service';
 
 describe('ServiceService', () => {
-  let service: ServiceService;
+  let service: ProdutoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ServiceService);
+    service = TestBed.inject(ProdutoService);
   });
 
   it('should be created', () => {
