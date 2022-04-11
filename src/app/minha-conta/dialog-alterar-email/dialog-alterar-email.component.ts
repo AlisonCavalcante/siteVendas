642a9d781560935d377c9c8ad828fb9e1fb0985a
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-11 10:56:43 -0300

    add verificando se os e-mails são iguais

--- src/app/minha-conta/dialog-alterar-email/dialog-alterar-email.component.ts
+++ src/app/minha-conta/dialog-alterar-email/dialog-alterar-email.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogAlterarEmailComponent implements OnInit {
 
+  data = {
+    email: '',
+    email_confirm: '',
+  };
+
   constructor() { }
 
   ngOnInit(): void {
