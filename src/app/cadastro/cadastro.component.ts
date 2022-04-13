db33ca0c6c8ee42b351bd2f3253b8978a9ee3160
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-13 10:04:12 -0300

    add focus no campo cpf e ajuste do método login

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,49 +1,60 @@
 import { User } from './../models/users.model';
 import { DataService } from './../data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
-
   usuario!: User[];
+  @ViewChild('cpfInput') cpfInput!: ElementRef<HTMLInputElement>;
   formulario1!: FormGroup;
   formulario2!: FormGroup;
 
-  constructor(private userService: DataService, private route: Router, private formBuilder: FormBuilder) { }
+  constructor(
+    private userService: DataService,
+    private route: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.formulario1 = this.formBuilder.group({
       cpf: [null, [Validators.required]],
-      senha: [null, [Validators.required, Validators.minLength(6)]]
+      senha: [null, [Validators.required, Validators.minLength(6)]],
     });
     this.formulario2 = this.formBuilder.group({
-      email: [null, [Validators.email,Validators.required]]
-    })
+      email: [null, [Validators.email, Validators.required]],
+    });
   }
-  cadastro(){
-    this.route.navigate(["cadastro/criarContaPage"])
+  cadastro() {
+    this.route.navigate(['cadastro/criarContaPage']);
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.formulario1.value);
-    this.userService.getUsuario(this.formulario1.value).subscribe((res)=> {
+    this.userService.getUsuario(this.formulario1.value).subscribe((res) => {
       this.usuario = res;
-     if(this.usuario[0].senha === this.formulario1.get('senha')?.value) {
-       alert('Usuário logado');
-       this.route.navigate(['/'])
-       this.userService.setUser(this.usuario);
-     }else
-     alert('Dados incorretos');
-     this.resetarCampos();
-    })
+        if(this.usuario.length != 0){
+          if (this.usuario[0].senha === this.formulario1.get('senha')?.value) {
+            alert('Usuário logado');
+            this.route.navigate(['/']);
+            this.userService.setUser(this.usuario);
+          } else{
+            alert('Senha incorreta');
+            this.resetarCampos();
+            this.cpfInput.nativeElement.focus();
+          }
+        }else{
+          alert('Cpf incorreto');
+          this.resetarCampos();
+          this.cpfInput.nativeElement.focus();
+        }
+    });
   }
   resetarCampos() {
     this.formulario1.reset();
   }
-
 }
