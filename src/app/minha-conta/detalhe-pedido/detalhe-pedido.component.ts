0d660743d6ca7104a2a90b9a0531ffeb7e7e2498
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-18 10:09:12 -0300

    create detalhe do pedido component

---
+++ src/app/minha-conta/detalhe-pedido/detalhe-pedido.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-pedido',
+  templateUrl: './detalhe-pedido.component.html',
+  styleUrls: ['./detalhe-pedido.component.css']
+})
+export class DetalhePedidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
