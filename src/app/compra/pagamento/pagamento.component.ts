97cbca3f83cdc361e452d4c601d2f46fb82998ac
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-18 17:37:23 -0300

    atualização pagamento component

--- src/app/compra/pagamento/pagamento.component.ts
+++ src/app/compra/pagamento/pagamento.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagamentoComponent implements OnInit {
 
+  meses: string[] = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+  ];
+  anos: string[] = [
+    '22',
+    '23',
+    '24',
+    '25',
+    '26',
+    '27',
+    '28',
+    '29',
+    '30',
+    '31',
+    '32',
+    '33',
+  ];
   constructor() { }
 
   ngOnInit(): void {
