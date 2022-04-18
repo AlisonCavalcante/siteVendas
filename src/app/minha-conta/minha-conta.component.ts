0630c5d9d0481c187d707d37045a33b553d5f360
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-18 10:22:46 -0300

    add logout no minha conta

--- src/app/minha-conta/minha-conta.component.ts
+++ src/app/minha-conta/minha-conta.component.ts
@@ -1,3 +1,4 @@
+import { AuthserviceService } from './../cadastro/services/authservice.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MinhaContaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthserviceService) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
 
 }
