555088b14c8e3d1592ebc3ff1bed0b4bba8536ae
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-19 00:20:46 -0300

    atualização tela pagamento

--- src/app/compra/compra-routing.module.ts
+++ src/app/compra/compra-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/auth/auth.guard';
 
 import { CestaComprasComponent } from './cesta-compras/cesta-compras.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
@@ -8,7 +9,7 @@ import { PagamentoComponent } from './pagamento/pagamento.component';
 
 const comprasRoutes: Routes = [
   { path: '', component: CestaComprasComponent },
-  { path: 'pagamento', component: PagamentoComponent },
+  { path: 'pagamento', component: PagamentoComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
