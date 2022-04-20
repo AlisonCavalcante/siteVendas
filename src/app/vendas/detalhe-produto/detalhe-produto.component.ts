48ff4d07c5ae2673d413e00c26795d411f56e3a2
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-19 22:11:31 -0300

    create método de add carrinho no localStorage

--- src/app/vendas/detalhe-produto/detalhe-produto.component.ts
+++ src/app/vendas/detalhe-produto/detalhe-produto.component.ts
@@ -9,44 +9,56 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detalhe-produto',
   templateUrl: './detalhe-produto.component.html',
-  styleUrls: ['./detalhe-produto.component.css']
+  styleUrls: ['./detalhe-produto.component.css'],
 })
 export class DetalheProdutoComponent implements OnInit {
-
   panelOpenState = false;
   meuFavorito: boolean = true;
   id!: number;
   cep!: string;
   enderecoEntrega!: Endereco;
-  produtos$!: Observable <Produtos[]>;
+  produtos$!: Observable<Produtos[]>;
+  produto!: Produtos [];
 
-  constructor(private route: Router, private activeRouter: ActivatedRoute, private produtoService: ProdutoService, private dataService: DataService) { }
+  constructor(
+    private route: Router,
+    private activeRouter: ActivatedRoute,
+    private produtoService: ProdutoService,
+    private dataService: DataService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.activeRouter.snapshot.params['id'];
     this.getProduto();
   }
-  favoritar(){
+  favoritar() {
     this.meuFavorito = !this.meuFavorito;
   }
-  getProduto(){
-   this.produtos$ = this.produtoService.buscarProdutoId(this.id);
-   console.log(this.produtos$);
+  getProduto() {
+    this.produtos$ = this.produtoService.buscarProdutoId(this.id);
+    console.log(this.produtos$);
   }
-  buscarCep(){
-   this.produtoService.consultarCep(this.cep).subscribe(res => {
-     this.enderecoEntrega = res;
-     console.log(this.enderecoEntrega)
-   })
+  buscarCep() {
+    this.produtoService.consultarCep(this.cep).subscribe((res) => {
+      this.enderecoEntrega = res;
+      console.log(this.enderecoEntrega);
+    });
   }
-  comprar(produto: Produtos[]){
-
+  comprar(produto: Produtos[]) {
     // this.route.navigateByUrl('/cesta', {
     //   state: this.produto
     // })
     this.dataService.setProduto(produto);
     this.route.navigate(['/compra']);
   }
-
-
+  addCarrinho(produto: Produtos[]) {
+    let carrinho = localStorage.getItem('Carrinho');
+    if (carrinho) {
+      this.produto = JSON.parse(carrinho);
+      this.produto.push(produto[0]);
+      localStorage.setItem('Carrinho', JSON.stringify(this.produto));
+    } else {
+      localStorage.setItem('Carrinho', JSON.stringify(produto));
+    }
+  }
 }
